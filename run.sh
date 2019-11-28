daa45169e2b61b0678ec3f9aed1df2e005103f8d
Joe Zujkowski <jzujkowski@gmail.com>
2019-11-27 23:26:25 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [[ "${LARAVEL}" = true ]]; then
   cd /code
   php artisan swoole:http start
 else
-	echo "[info] No run command set.  Running default swoole.php (example app)"
-  cd /swoole
+  echo "[info] No run command set.  Running default swoole.php (example app)"
+  cd /app
   php swoole.php
 fi
